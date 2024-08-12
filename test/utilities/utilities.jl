ff00c6917f86fa14b9dae622de3d9f5417296b3c
Oliver Niehuis <oliver.niehuis@gmail.com>
2024-08-13 00:08:05 +0200

    Updated unit tests

--- test/utilities/utilities.jl
+++ test/utilities/utilities.jl
@@ -2,14 +2,14 @@ using JuChrom
 using Test
 using Unitful: 𝐓
 
-@testset "copy_with_eltype" begin
-   @test [1, 2, 3, 4, 5, 6] == JuChrom.copy_with_eltype(Int[1, 2, 3, 4, 5, 6], Float64)
-   @test [1, 2, 3, 4, 5, 6] == JuChrom.copy_with_eltype(Float64[1, 2, 3, 4, 5, 6], Int)
-   @test Vector{Float64} == typeof(JuChrom.copy_with_eltype(Int[1, 2, 3, 4, 5, 6], Float64))
-   @test Vector{Float64} == typeof(JuChrom.copy_with_eltype(1:6, Float64))
-   @test Vector{Int} == typeof(JuChrom.copy_with_eltype(1.0:6.0, Int))
-   @test Vector{Int} == typeof(JuChrom.copy_with_eltype(Float64[1, 2, 3, 4, 5, 6], Int))
-   @test Matrix{Int} == typeof(JuChrom.copy_with_eltype(Float64[1 2; 3 4; 5 6], Int))
-   @test Matrix{Float64} == typeof(JuChrom.copy_with_eltype(Int[1 2; 3 4; 5 6], Float64))
-   @test_throws InexactError JuChrom.copy_with_eltype(Float64[1.1, 2, 3, 4, 5, 6], Int)
-end
+# @testset "copy_with_eltype" begin
+#    @test [1, 2, 3, 4, 5, 6] == JuChrom.copy_with_eltype(Int[1, 2, 3, 4, 5, 6], Float64)
+#    @test [1, 2, 3, 4, 5, 6] == JuChrom.copy_with_eltype(Float64[1, 2, 3, 4, 5, 6], Int)
+#    @test Vector{Float64} == typeof(JuChrom.copy_with_eltype(Int[1, 2, 3, 4, 5, 6], Float64))
+#    @test Vector{Float64} == typeof(JuChrom.copy_with_eltype(1:6, Float64))
+#    @test Vector{Int} == typeof(JuChrom.copy_with_eltype(1.0:6.0, Int))
+#    @test Vector{Int} == typeof(JuChrom.copy_with_eltype(Float64[1, 2, 3, 4, 5, 6], Int))
+#    @test Matrix{Int} == typeof(JuChrom.copy_with_eltype(Float64[1 2; 3 4; 5 6], Int))
+#    @test Matrix{Float64} == typeof(JuChrom.copy_with_eltype(Int[1 2; 3 4; 5 6], Float64))
+#    @test_throws InexactError JuChrom.copy_with_eltype(Float64[1.1, 2, 3, 4, 5, 6], Int)
+# end
