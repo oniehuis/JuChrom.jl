46ee68f152a87d04cb6632a6c4b5535ddf4429af
Oliver Niehuis <oliver.niehuis@gmail.com>
2024-08-13 00:05:27 +0200

    Excluded non-public function from documentation

--- docs/src/index.md
+++ docs/src/index.md
@@ -4,5 +4,6 @@ Documentation for JuChrom.jl
 
 ```@autodocs
 Modules = [JuChrom]
+Private = false
 Order   = [:type, :function]
 ```
--- src/utilities.jl
+++ src/utilities.jl
@@ -1,34 +1,33 @@
-"""
-    copy_with_eltype(array::AbstractArray, elementtype::Type)
+# """
+#     JuChrom.copy_with_eltype(array::AbstractArray, elementtype::Type)
 
-Create a mutable copy of the `array` with the type of its elements converted to 
-`elementtype`.
+# Create a mutable copy of the `array` with the type of its elements converted to 
+# `elementtype`.
 
-# Example
-```jldoctest
-julia> JuChrom.copy_with_eltype(Int[1, 2, 3, 4, 5, 6], Float64)
-6-element Vector{Float64}:
- 1.0
- 2.0
- 3.0
- 4.0
- 5.0
- 6.0
+# # Example
+# ```jldoctest
+# julia> JuChrom.copy_with_eltype(Int[1, 2, 3, 4, 5, 6], Float64)
+# 6-element Vector{Float64}:
+#  1.0
+#  2.0
+#  3.0
+#  4.0
+#  5.0
+#  6.0
 
-julia> JuChrom.copy_with_eltype(Float64[1, 2, 3, 4, 5, 6], Int32)
-6-element Vector{Int32}:
- 1
- 2
- 3
- 4
- 5
- 6
+# julia> JuChrom.copy_with_eltype(Float64[1, 2, 3, 4, 5, 6], Int32)
+# 6-element Vector{Int32}:
+#  1
+#  2
+#  3
+#  4
+#  5
+#  6
 
-julia> JuChrom.copy_with_eltype(Float64[1.1, 2, 3, 4, 5, 6], Int32)
-ERROR: InexactError: Int32(1.1)
-[...]
-
-```
-"""
-copy_with_eltype(array::AbstractArray, elementtype::Type) = copyto!(similar(array, 
-    elementtype), array)
+# julia> JuChrom.copy_with_eltype(Float64[1.1, 2, 3, 4, 5, 6], Int32)
+# ERROR: InexactError: Int32(1.1)
+# [...]
+# ```
+# """
+# copy_with_eltype(array::AbstractArray, elementtype::Type) = copyto!(similar(array, 
+#     elementtype), array)
