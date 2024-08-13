f84c412366505fbf36b78e68645a9baf5c3e7fc6
Oliver Niehuis <oliver.niehuis@gmail.com>
2024-08-13 23:01:12 +0200

    Removed Latex from documentation

--- src/utilities.jl
+++ src/utilities.jl
@@ -68,10 +68,7 @@ end
     integer(value:::Real; start::Real=0.7) -> Int
 
 Return the integer for the given `value` that satisfies the following condition: 
-``integer - 1 + start ≤ value < integer + start``, where ``0 ≤ start < 1``.
-
-``$\frac{x}{y}$``
-
+integer - 1 + start ≤ value < integer + start, where 0 ≤ start < 1.
 
 See also [`AbstractGCMS`](@ref), [`GCMS`](@ref), [`binions`](@ref), [`ions`](@ref).
 
