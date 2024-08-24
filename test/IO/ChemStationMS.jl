6fddbe892f8a0fb81490d435dd6a2e028bed0eda
Oliver Niehuis <oliver.niehuis@gmail.com>
2024-08-24 14:46:11 +0200

    Enhanced unit testing

--- test/IO/ChemStationMS.jl
+++ test/IO/ChemStationMS.jl
@@ -3,9 +3,8 @@ using Test
 
 @testset "ChemStationMS" begin
     @test "data.ms" == JuChrom.InputOutput.ChemStationMSReaders.ChemStationMS().datafilename
-    @test "datasim.ms" == JuChrom.InputOutput.ChemStationMSReaders.ChemStationMS(; datafilename="datasim.ms").datafilename
-
-    # Check supertype
+    @test "datasim.ms" == JuChrom.InputOutput.ChemStationMSReaders.ChemStationMS(; 
+        datafilename="datasim.ms").datafilename
     @test isa(JuChrom.InputOutput.ChemStationMSReaders.ChemStationMS(), FileFormat)
 end
 
@@ -31,5 +30,27 @@ end
         "5176 ions; range: m/z 29.0 - 562.9\n",
         "intensity range: 0 - 1186816\n",
         "metadata: 0 entries")
+
+    # IOError: unsupported source for ChemStationMS data import, because file does not 
+    # exist
+    dfile = joinpath(JuChrom.agilent, "C7-C40_ChemStationMS.D", "data2.ms")
+    @test_throws JuChrom.InputOutput.IOError importdata(dfile, 
+        JuChrom.InputOutput.ChemStationMSReaders.ChemStationMS())
+    
+    # IOError: unsupported source for ChemStationMS data import, because specified D. 
+    # folder does not exist
+    dfolder = joinpath(JuChrom.agilent, "C7-C40_ChemStationMS.D", "imaginary")
+    @test_throws JuChrom.InputOutput.IOError importdata(dfolder, 
+        JuChrom.InputOutput.ChemStationMSReaders.ChemStationMS())
+
+    # IOError: not an Agilent .D folder, because folder exists, but does not end with .D
+    dfolder = JuChrom.agilent
+    @test_throws JuChrom.InputOutput.IOError importdata(dfolder, 
+        JuChrom.InputOutput.ChemStationMSReaders.ChemStationMS())
+
+    # target file not in D. folder
+    dfolder = joinpath(JuChrom.agilent, "C7-C40_ChemStationMS.D")
+    @test_throws JuChrom.InputOutput.IOError importdata(dfolder, 
+        JuChrom.InputOutput.ChemStationMSReaders.ChemStationMS(datafilename="datasim.ms"))
 end
 
