077724f68610807633e12e8b2f49660f6699bf71
Oliver Niehuis <oliver.niehuis@gmail.com>
2024-08-28 20:59:32 +0200

    Enhanced unit testing for bsplineinterpolation

--- test/utilities/utilities.jl
+++ test/utilities/utilities.jl
@@ -19,12 +19,20 @@ using Test
 
     @test_throws ArgumentError bsplineinterpolation([1, 2, 3]u"s", [1000, 1900, 3100], 
         bsplineorder=4)
-    @test_throws ArgumentError bsplineinterpolation([1, 2, 3, 4, 5]u"s", [1900, 1000, 3100, 3900])
-    @test_throws ArgumentError bsplineinterpolation([1, 2, 3, 4]u"s", [1900, 1000, 3100, 3900, 4000])
-    @test_throws ArgumentError bsplineinterpolation([1, 1, 3, 4]u"s", [1000, 1900, 3100, 3900])
-    @test_throws ArgumentError bsplineinterpolation([2, 1, 3, 4]u"s", [1000, 1900, 3100, 3900])
-    @test_throws ArgumentError bsplineinterpolation([1, 2, 3, 4]u"s", [1000, 1000, 3100, 3900])
-    @test_throws ArgumentError bsplineinterpolation([1, 2, 3, 4]u"s", [1900, 1000, 3100, 3900])
+    @test_throws ArgumentError bsplineinterpolation([1, 2, 3, 4, 5]u"s", 
+        [1900, 1000, 3100, 3900])
+    @test_throws ArgumentError bsplineinterpolation([1, 2, 3, 4]u"s", 
+        [1900, 1000, 3100, 3900, 4000])
+    @test_throws ArgumentError bsplineinterpolation([1, 1, 3, 4]u"s", 
+        [1000, 1900, 3100, 3900])
+    @test_throws ArgumentError bsplineinterpolation([2, 1, 3, 4]u"s", 
+        [1000, 1900, 3100, 3900])
+    @test_throws ArgumentError bsplineinterpolation([1, 2, 3, 4]u"s", 
+        [1000, 1000, 3100, 3900])
+    @test_throws ArgumentError bsplineinterpolation([1, 2, 3, 4]u"s", 
+        [1900, 1000, 3100, 3900])
+    @test_throw ArgumentError bsplineinterpolation([1, 2, 3, 4]u"s", 
+        [1000, 1900, 3100, 3900], extrapolation=:Smooth)
 end
 
 ############################################################################################
@@ -33,7 +41,8 @@ end
 @testset "copy_with_eltype" begin
    @test [1, 2, 3, 4, 5, 6] == JuChrom.copy_with_eltype(Int[1, 2, 3, 4, 5, 6], Float64)
    @test [1, 2, 3, 4, 5, 6] == JuChrom.copy_with_eltype(Float64[1, 2, 3, 4, 5, 6], Int)
-   @test Vector{Float64} == typeof(JuChrom.copy_with_eltype(Int[1, 2, 3, 4, 5, 6], Float64))
+   @test Vector{Float64} == typeof(JuChrom.copy_with_eltype(Int[1, 2, 3, 4, 5, 6], 
+    Float64))
    @test Vector{Float64} == typeof(JuChrom.copy_with_eltype(1:6, Float64))
    @test Vector{Int} == typeof(JuChrom.copy_with_eltype(1.0:6.0, Int))
    @test Vector{Int} == typeof(JuChrom.copy_with_eltype(Float64[1, 2, 3, 4, 5, 6], Int))
