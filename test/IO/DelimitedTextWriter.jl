c5689be8ef81dd3af623d006793bd4932af3ee73
Oliver Niehuis <oliver.niehuis@gmail.com>
2024-09-04 16:36:01 +0200

    Added unit testing

--- test/IO/DelimitedTextWriter.jl
+++ test/IO/DelimitedTextWriter.jl
@@ -4,9 +4,23 @@ using DelimitedFiles
 
 @testset "DelimitedTextWriter" begin
     rootpath = joinpath(splitpath(pathof(JuChrom))[begin:end-2])
-    pathname = joinpath(rootpath, "tmp", "delimtest")
+    path = joinpath(rootpath, "tmp")
+    mkpath(path)
+    file = joinpath(path, "delimtest")
     chrom = Chrom((1:2)u"minute", [123, 224])
-    exportdata(chrom, pathname, DelimitedText(), overwrite=true)
-    data = readdlm(string(pathname, ".tsv"), '\t')
+    exportdata(chrom, file, DelimitedText(), overwrite=true)
+    data = readdlm(string(file, ".tsv"), '\t')
+    @test data == Any["scan time [minute]" "intensity"; 1 123; 2 224]
+
+    exportdata(chrom, file, DelimitedText(delim=","), overwrite=true)
+    data = readdlm(string(file, ".csv"), ',')
+    @test data == Any["scan time [minute]" "intensity"; 1 123; 2 224]
+
+    exportdata(chrom, file, DelimitedText(delim=";"), overwrite=true)
+    data = readdlm(string(file, ".csv"), ';')
+    @test data == Any["scan time [minute]" "intensity"; 1 123; 2 224]
+
+    exportdata(chrom, file, DelimitedText(delim="*"), overwrite=true)
+    data = readdlm(string(file, ".txt"), '*')
     @test data == Any["scan time [minute]" "intensity"; 1 123; 2 224]
 end
