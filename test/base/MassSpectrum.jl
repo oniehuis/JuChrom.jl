2c09724533f6454a6903ec070081f08f37e08db5
Oliver Niehuis <oliver.niehuis@gmail.com>
2024-09-11 21:55:15 +0200

    enhanced unit testing

--- test/base/MassSpectrum.jl
+++ test/base/MassSpectrum.jl
@@ -45,117 +45,117 @@ using Unitful: 𝐓
         retentionindexname=SubString("Kovats", 1, 6), retentionindex=131
         ).retentionindexname)       
 
+    # Retention index name
     @test 131 == MassSpectrum([85.1, 112.2, 124.1], [13, 0, 67], 
         retentionindexname="Kovats", retentionindex=131).retentionindex
+    @test Int == typeof(MassSpectrum([85.1, 112.2, 124.1], [13, 0, 67], 
+        retentionindexname="Kovats", retentionindex=Int(131)).retentionindex)
+    @test Float64 == typeof(MassSpectrum([85.1, 112.2, 124.1], [13, 0, 67], 
+        retentionindexname="Kovats", retentionindex=131.0).retentionindex)
+
+    # Metadata
     @test Dict(:test => 1) == MassSpectrum([85.1, 112.2, 124.1], [13, 0, 67], 
         metadata=Dict(:test => 1)).metadata
+    @test Dict{Any, Any} == typeof(MassSpectrum([85.1, 112.2, 124.1], [13, 0, 67], 
+        metadata=Dict(:test => 1)).metadata)
+    
+    # Check the associated supertypes
+    @test isa(MassSpectrum([85.1, 112.2, 124.1], [13, 0, 67]), AbstractMassSpectrum)
+
+    # Must provide at least one ion
+    @test_throws ArgumentError MassSpectrum(Int[], Int[])
+    @test [1] == MassSpectrum([1], [1]).ions
+
+    # Number of scantimes and intensities must be identical
+    @test_throws DimensionMismatch MassSpectrum([1], [1, 2])
+    @test_throws DimensionMismatch MassSpectrum([1, 2], [1])
+
+    # Ions must be in ascending order and cannot have identical values
+    @test_throws ArgumentError MassSpectrum([2, 1], [1, 2])
+    @test_throws ArgumentError MassSpectrum([1, 1], [1, 2])
+
+    # Intensity values cannot be less than zero
+    @test_throws ArgumentError MassSpectrum([1, 2, 3], [-12, 956, 23])
+
+    # If the retention index is given, the retention index name must also be given
+    @test_throws ArgumentError MassSpectrum([85.1, 112.2, 124.1], [13, 0, 67], 
+        retentionindex=131)
+
+    # If the retention index name is given, the retention index  must also be given
+    @test_throws ArgumentError MassSpectrum([85.1, 112.2, 124.1], [13, 0, 67], 
+        retentionindexname="Kovats")
+
+    # If the retention index name must be some type of AstractString
+    @test_throws MethodError MassSpectrum([85.1, 112.2, 124.1], [13, 0, 67], 
+        retentionindexname=:Kovats)
+
+    # Metadata cannot be anything other than a dictionary.
+    @test_throws TypeError MassSpectrum([85.1, 112.2, 124.1], [13, 0, 67], 
+        metadata=:sample_name)
+    @test_throws TypeError MassSpectrum([85.1, 112.2, 124.1], [13, 0, 67], 
+        metadata=1)
+    @test_throws TypeError MassSpectrum([85.1, 112.2, 124.1], [13, 0, 67], 
+        metadata='S')
+
+    # MassSpectrum is broadcastable
+    @test [85.1, 112.2] == ((ms, i) -> ms.ions[i]).(MassSpectrum([85.1, 112.2, 124.1], 
+        [13, 0, 67]), [1, 2])
 end
 
-# # Verify object construction and field types depending on constructor arguments
-#     # Scantimes
-#     @test [1, 2, 3]u"s" == Chrom([1, 2, 3]u"s", [12, 956, 23]).scantimes
-#     @test Vector{Quantity{Int64, 𝐓 , Unitful.FreeUnits{(Unitful.Unit{:Second, 𝐓}(0, 
-#         1//1),), 𝐓 , nothing}}} == typeof(Chrom(Int64[1, 2, 3]u"s", [12, 956, 23]
-#         ).scantimes)
-#     @test Vector{Quantity{Float64, 𝐓 , Unitful.FreeUnits{(Unitful.Unit{:Second, 𝐓}(0, 
-#         1//1),), 𝐓 , nothing}}} == typeof(Chrom(Float64[1, 2, 3]u"s", 
-#             [12, 956, 23]).scantimes)
-
-#     # Intensities
-#     @test [12, 956, 23] == Chrom([1, 2, 3]u"s", [12, 956, 23]).intensities
-#     @test Vector{Int64} == typeof(Chrom([1, 2, 3]u"s", Int64[12, 956, 23]).intensities)
-#     @test Vector{Float64} == typeof(Chrom([1, 2, 3]u"s",  Float64[12, 956, 23]
-#         ).intensities)
-
-#     # Metadata
-#     @test Dict{Any, Any} == typeof(Chrom([1, 2, 3]u"s", [12, 956, 23]).metadata)
-#     @test Dict() == Chrom([1, 2, 3]u"s", [12, 956, 23]).metadata
-#     @test Dict(:id => 4, "name" => "sample") == Chrom([1, 2, 3]u"s", [12, 956, 23], 
-#         Dict(:id => 4, "name" => "sample")).metadata
-
-#     # RiMapper
-#     @test isa(Chrom([1, 2, 3]u"s", [12, 956, 23]).rimapper, Nothing)
-#     @test isa(Chrom([1, 2, 3]u"s", [12, 956, 23], rimapper=RiMapper("Kovats", 
-#         (1:5)u"minute", 1000:1000:5000)).rimapper, JuChrom.AbstractRiMapper)
-
-#     # Check the associated supertypes
-#     @test isa(Chrom([1, 2, 3]u"s", [12, 956, 23]), AbstractChromatogram)
-#     @test isa(Chrom([1, 2, 3]u"s", [12, 956, 23]), AbstractChrom)
-
-#     # Scan time vector accepts only time values
-#     @test_throws MethodError Chrom([1, 2, 3], [12, 956, 23])
-#     @test_throws MethodError Chrom([1, 2, 3]u"m", [12, 956, 23])
-    
-#     # Intensity vector must not accept a unit
-#     @test_throws MethodError Chrom([1, 2, 3], [12, 956, 23]u"s")
-
-#     # Must provide at least one scantime
-#     @test_throws ArgumentError Chrom(Int[]u"s", Int[])
-#     @test_throws ArgumentError Chrom(Int[]u"s", [12])
-#     @test [1]u"s" == Chrom([1]u"s", [12]).scantimes
-
-#     # Must provide at least on intensity
-#     @test_throws ArgumentError Chrom([1]u"s", Int[])
-
-#     # Metadata cannot be anything other than a dictionary.
-#     @test_throws MethodError Chrom([1, 2, 3]u"s", [12, 956, 23], :sample_name)
-#     @test_throws MethodError Chrom([1, 2, 3]u"s", [12, 956, 23], 1)
-#     @test_throws MethodError Chrom([1, 2, 3]u"s", [12, 956, 23], 'S')
-
-#     # Number of scantimes and intensities must be identical
-#     @test_throws DimensionMismatch Chrom([1, 2, 3, 4]u"s", [12, 956, 23])
-#     @test_throws DimensionMismatch Chrom([1, 2]u"s", [12, 956, 23])
-
-#     # Scan times must be in ascending order
-#     @test_throws ArgumentError Chrom([2, 1, 3]u"s", [12, 956, 23])
-#     @test_throws ArgumentError Chrom([2, 2, 3]u"s", [12, 956, 23])
-
-#     # Intensity values cannot be less than zero
-#     @test_throws ArgumentError Chrom([1, 2, 3]u"s", [-12, 956, 23])
-
-#     # Chrom is broadcastable
-#     @test [1, 2]u"s" == ((chrom, i) -> chrom.scantimes[i]).(Chrom([1, 2, 3]u"s", 
-#         [12, 956, 23]), [1, 2])
-# end
-
-
-# @testset "show Chrom" begin
-#     io = IOBuffer()
-#     show(io, Chrom(Int64[1]u"s", Int64[12]))
-#     @test String(take!(io)) == string(
-#         "JuChrom.Chrom {scan times: Int64, intensities: Int64}\n",
-#         "1 scan; scan time: 1 s\n",
-#         "intensity: 12\n",
-#         "metadata: 0 entries")
-#     show(io, Chrom(Int64[1, 2]u"s", Int64[12, 13]))
-#     @test String(take!(io)) == string(
-#         "JuChrom.Chrom {scan times: Int64, intensities: Int64}\n",
-#         "2 scans; scan times: 1 s, 2 s\n",
-#         "intensity range: 12 - 13\n",
-#         "metadata: 0 entries")
-#     show(io, Chrom(convert(Vector{Int64}, (1:10))u"s", convert(Vector{Int64}, (12:21))))
-#     @test String(take!(io)) == string(
-#         "JuChrom.Chrom {scan times: Int64, intensities: Int64}\n",
-#         "10 scans; scan times: 1 s, 2 s, 3 s, 4 s, 5 s, 6 s, 7 s, 8 s, 9 s, 10 s\n",
-#         "intensity range: 12 - 21\n",
-#         "metadata: 0 entries")
-#     show(io, Chrom(convert(Vector{Int64}, (1:11))u"s", convert(Vector{Int64}, (12:22))))
-#     @test String(take!(io)) == string(
-#         "JuChrom.Chrom {scan times: Int64, intensities: Int64}\n",
-#         "11 scans; scan time range: 1 s - 11 s\n",
-#         "intensity range: 12 - 22\n",
-#         "metadata: 0 entries")
-#     show(io, Chrom(Int64[1, 2, 3]u"s", Int64[12, 956, 23], Dict(:id => 4)))
-#     @test String(take!(io)) == string(
-#         "JuChrom.Chrom {scan times: Int64, intensities: Int64}\n",
-#         "3 scans; scan times: 1 s, 2 s, 3 s\n",
-#         "intensity range: 12 - 956\n",
-#         "metadata: 1 entry")
-#     show(io, Chrom(Int64[1, 2, 3]u"s", Int64[12, 956, 23], Dict(:id => 4, 
-#         "name" => "sample")))
-#     @test String(take!(io)) == string(
-#         "JuChrom.Chrom {scan times: Int64, intensities: Int64}\n",
-#         "3 scans; scan times: 1 s, 2 s, 3 s\n",
-#         "intensity range: 12 - 956\n",
-#         "metadata: 2 entries")
-# end
+@testset "show AbstractMassSpectrum" begin
+    io = IOBuffer()
+    show(io, MassSpectrum([85.1, 112.2, 124.1], Int64[13, 0, 67]))
+    @test String(take!(io)) == string(
+        "JuChrom.MassSpectrum {ions: Float64, intensities: Int64}\n",
+        "3 ions: m/z 85.1, 112.2, 124.1\n",
+        "intensity range: 0 - 67\n",
+        "metadata: 0 entries")
+    show(io, MassSpectrum([85.1], Int64[13]))
+    @test String(take!(io)) == string(
+        "JuChrom.MassSpectrum {ions: Float64, intensities: Int64}\n",
+        "1 ion: m/z 85.1\n",
+        "intensity: 13\n",
+        "metadata: 0 entries")
+    show(io, MassSpectrum(convert(Vector{Int64}, collect(1:11)), convert(Vector{Int64}, 
+        collect(1:11))))
+    @test String(take!(io)) == string(
+        "JuChrom.MassSpectrum {ions: Int64, intensities: Int64}\n",
+        "11 ions; range: m/z 1 - 11\n",
+        "intensity range: 1 - 11\n",
+        "metadata: 0 entries")
+    show(io, MassSpectrum([85.1], Int64[13], retentiontime=1.0u"s"))
+    @test String(take!(io)) == string(
+        "JuChrom.MassSpectrum {ions: Float64, intensities: Int64}\n",
+        "1 ion: m/z 85.1\n",
+        "intensity: 13\n",
+        "retention time: 1.0 s\n",
+        "metadata: 0 entries")
+    show(io, MassSpectrum([85.1], Int64[13], retentiontime=1.0u"s", 
+        retentionindexname="Kovats", retentionindex=123.2))
+    @test String(take!(io)) == string(
+        "JuChrom.MassSpectrum {ions: Float64, intensities: Int64}\n",
+        "1 ion: m/z 85.1\n",
+        "intensity: 13\n",
+        "retention time: 1.0 s\n",
+        "retention index: 123.2 (Kovats)\n",
+        "metadata: 0 entries")
+    show(io, MassSpectrum([85.1], Int64[13], retentiontime=1.0u"s", 
+        retentionindexname="Kovats", retentionindex=123.2, metadata=Dict(:test => 1)))
+    @test String(take!(io)) == string(
+        "JuChrom.MassSpectrum {ions: Float64, intensities: Int64}\n",
+        "1 ion: m/z 85.1\n",
+        "intensity: 13\n",
+        "retention time: 1.0 s\n",
+        "retention index: 123.2 (Kovats)\n",
+        "metadata: 1 entry")
+    show(io, MassSpectrum([85.1], Int64[13], retentiontime=1.0u"s", 
+        retentionindexname="Kovats", retentionindex=123.2, metadata=Dict(:test => 1, 
+        2 => 3)))
+    @test String(take!(io)) == string(
+        "JuChrom.MassSpectrum {ions: Float64, intensities: Int64}\n",
+        "1 ion: m/z 85.1\n",
+        "intensity: 13\n",
+        "retention time: 1.0 s\n",
+        "retention index: 123.2 (Kovats)\n",
+        "metadata: 2 entries")
+end
