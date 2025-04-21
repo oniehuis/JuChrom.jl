c0f3b9210364efe896077a8116e0a7e856daa4f5
Oliver Niehuis <oliver.niehuis@biologie.uni-freiburg.de>
2025-04-21 23:17:32 +0200

    Testing

--- src/JuChrom.jl
+++ src/JuChrom.jl
@@ -1,15 +1,5 @@
 module JuChrom
 
-using Reexport
-
-using Pkg.Artifacts
-
-@reexport using Unitful
-
-const agilent = artifact"Agilent"
-const andi = artifact"ANDI"
-const calibration = artifact"calibration"
-const shimadzu = artifact"Shimadzu"
-
+# Contents
 
 end
