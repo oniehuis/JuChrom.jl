d8eba2bdc3d7ffa4846dccae5f1e56841d67846f
Oliver Niehuis <oliver.niehuis@biologie.uni-freiburg.de>
2025-04-21 23:28:50 +0200

    Testing

--- src/JuChrom.jl
+++ src/JuChrom.jl
@@ -11,4 +11,9 @@ import Roots
 
 @reexport using Unitful
 
+const agilent = artifact"Agilent"
+const andi = artifact"ANDI"
+const calibration = artifact"calibration"
+const shimadzu = artifact"Shimadzu"
+
 end
