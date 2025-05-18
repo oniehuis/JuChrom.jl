e165d369694422caea52089d7b1a17ce1bfd7dd7
Oliver Niehuis <oliver.niehuis@biologie.uni-freiburg.de>
2025-05-18 14:58:44 +0200

    Testing

--- src/JuChrom.jl
+++ src/JuChrom.jl
@@ -98,21 +98,21 @@ export ShimadzuMS
 export exportdata
 export importdata
 
-include("NNLS/NNLS.jl")
-using .NNLS
+# include("NNLS/NNLS.jl")
+# using .NNLS
 
 include("Deconvolution/Deconvolution.jl")
 using .Deconvolution
 
-export candidatepeaks
-export saveplot
-export deconvolutedtic
-export componentbins
-export componentpeakmodels
-export massspectra
+# export candidatepeaks
+# export saveplot
+# export deconvolutedtic
+# export componentbins
+# export componentpeakmodels
+# export massspectra
 export nextlocalmaximum
 export LocalMaxima
-export deconvolute
+# export deconvolute
 export sigma
 
 # include("GUI/Explorer.jl")
